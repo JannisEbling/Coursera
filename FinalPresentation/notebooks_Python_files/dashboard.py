1ef46ea20e62226456fafd45b2adba28f1b0aa01
JannisEbling <65505435+JannisEbling@users.noreply.github.com>
2022-11-17 22:43:04 +0100

    Update dashboard.py

--- FinalPresentation/notebooks_Python_files/dashboard.py
+++ FinalPresentation/notebooks_Python_files/dashboard.py
@@ -1,3 +1,20 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Create an app layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
@@ -48,26 +65,27 @@ def get_pie_chart(entered_site):
         title='total launches')
         return fig
     else:
-        fig = px.pie(filtered_df, values='class',
-        names='class', 
-        title='Successrate of launches')
+        # return the outcomes piechart for a selected site
+        filtered_df=spacex_df[spacex_df['Launch Site']== entered_site]
+        filtered_df=filtered_df.groupby(['Launch Site','class']).size().reset_index(name='class count')
+        fig=px.pie(filtered_df,values='class count',names='class',title="Successrate")
         return fig
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
-            Input(component_id='site-dropdown', component_property='value1'), 
-            Input(component_id="payload-slider", component_property="value2"))
+            Input(component_id='site-dropdown', component_property='value'), 
+            Input(component_id="payload-slider", component_property="value"))
 def get__chart(entered_site, payload):
     
-    if entered_site == 'ALL':
-        fig = px.scatter(spacex_df, x=payload,y = "class",  
-        title='Scatterplot of Payload and class')
+    filtered_df = spacex_df[spacex_df['Payload Mass (kg)'].between(payload[0],payload[1])]
+    # thought reusing filtered_df may cause issues, but tried it out of curiosity and it seems to be working fine
+    
+    if entered_site=='ALL':
+        fig=px.scatter(filtered_df,x='Payload Mass (kg)',y='class',color='Booster Version Category',title='Success count for Payload mass')
         return fig
     else:
-        filtered_df = spacex_df[spacex_df["Launch Site"] == entered_site]
-        fig = px.scatter(filtered_df,x = payload,y = "class",color="Booster Version Category",  
-        title='Scatterplot of Payload and class')
+        fig=px.scatter(filtered_df[filtered_df['Launch Site']==entered_site],x='Payload Mass (kg)',y='class',color='Booster Version Category',title="Success count for Payload ")
         return fig
 # Run the app
 if __name__ == '__main__':
